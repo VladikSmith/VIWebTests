e7fd75c75b8ad7737ae6fe45e53516921980ddff
Vladislav Kuznetsov <vladiksmith@yandex.ru>
2026-03-30 20:17:20 +0300

    добавлен allure и функция со скриншотом

--- tests/LoginTests.py
+++ tests/LoginTests.py
@@ -1,18 +1,23 @@
 from core.BaseTest import browser
 from pages.BasePage import BasePage
 from pages.LoginPage import LoginPageHelper
+import allure
 
 BASE_URL = 'https://ok.ru/'
 EMPTY_LOGIN_ERROR = 'Login required'
 EMPTY_PASSWORD_ERROR = 'Password required'
 
 
+@allure.suite('Проверка формы авторизации')
+@allure.title('Проверка ошибки при пустой форме авторизации')
 def test_empty_login_and_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.click_login()
     assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
 
+
+@allure.title('Проверка ошибки при авторизации без пароля')
 def test_empty_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
