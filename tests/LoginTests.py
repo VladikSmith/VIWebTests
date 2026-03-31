731fb766ae98d59817e13106e45d928e143c4742
Vladislav Kuznetsov <vladiksmith@yandex.ru>
2026-03-31 17:42:32 +0300

    добавил time.sleep(5) в автотесты

--- tests/LoginTests.py
+++ tests/LoginTests.py
@@ -2,6 +2,7 @@ from core.BaseTest import browser
 from pages.BasePage import BasePage
 from pages.LoginPage import LoginPageHelper
 import allure
+import time
 
 BASE_URL = 'https://ok.ru/'
 EMPTY_LOGIN_ERROR = 'Login required'
@@ -14,6 +15,7 @@ def test_empty_login_and_password(browser):
     BasePage(browser).get_url(BASE_URL)
     LoginPage = LoginPageHelper(browser)
     LoginPage.click_login()
+    time.sleep(5)
     assert LoginPage.get_error_text() == EMPTY_LOGIN_ERROR
 
 
@@ -23,4 +25,5 @@ def test_empty_password(browser):
     LoginPage = LoginPageHelper(browser)
     LoginPage.fill_login_field()
     LoginPage.click_login()
+    time.sleep(5)
     assert LoginPage.get_error_text() == EMPTY_PASSWORD_ERROR
