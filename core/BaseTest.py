4511111f1f7a256cf65feca5bc37055cd9f46e17
Vladislav Kuznetsov <vladiksmith@yandex.ru>
2026-05-05 19:36:54 +0300

    в BaseTest добавлен webdriver.Remote

--- core/BaseTest.py
+++ core/BaseTest.py
@@ -8,6 +8,6 @@ def browser():
     chrome_options = Options()
     chrome_options.add_argument('--lang=ru')
     chrome_options.add_argument('--start-maximized')
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Remote(command_executor='http://83.222.25.223:4444',options=chrome_options)
     yield driver
     driver.quit()
